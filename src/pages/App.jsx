416a3a908601b9077fde6c231a2b2dd7d2678c6e
Christopher Landas <Landas.229246@fairview.sti.edu.ph>
2024-08-21 16:44:39 +0800

    parallax v2

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,64 +3,34 @@ import styles from '../styles/App.module.css'
 
 import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax"
 
-import img1 from '../assets/parallax backgound pack/1.png'
-import img2 from '../assets/parallax backgound pack/2.png'
-import img3 from '../assets/parallax backgound pack/3.png'
-import img4 from '../assets/parallax backgound pack/4.png'
-import img5 from '../assets/parallax backgound pack/5.png'
-import img6 from '../assets/parallax backgound pack/6.png'
-import img7 from '../assets/parallax backgound pack/7.png'
-import img8 from '../assets/parallax backgound pack/8.png'
-import img9 from '../assets/parallax backgound pack/9.png'
-import img10 from '../assets/parallax backgound pack/10.png'
-import img11 from '../assets/parallax backgound pack/11.png'
+import l1 from '../assets/Island/L1.png'
+import l2 from '../assets/Island/L2.png'
+import l3 from '../assets/Island/L3.png'
+import l4 from '../assets/Island/L4.png'
+import l5 from '../assets/Island/L5.png'
 
 const App = () => {
   return (
     <>
       <Header/>
       <div className={styles.main}>
-        {/* <ParallaxProvider>
-          <Parallax speed={-20}>
-            <div style={{backgroundImage: `url(${img1})`, height: '100vh', position:'absolute', top: '0', bottom: '0'}}></div>
-          </Parallax>
-
-          <Parallax speed={-17}>
-            <div style={{backgroundImage: `url(${img2})`, height: '100vh', position:'absolute', top: '0', bottom: '0'}}></div>
-          </Parallax>
-
-          <Parallax speed={-13}>
-            <div style={{backgroundImage: `url(${img3})`, height: '100vh', position:'absolute', top: '0', bottom: '0'}}></div>
-          </Parallax>
-
-          <Parallax speed={-12}>
-            <div style={{backgroundImage: `url(${img4})`, height: '100vh', position:'absolute', top: '0', bottom: '0'}}></div>
-          </Parallax>
-
-          <Parallax speed={-20}>
-            <div style={{backgroundImage: `url(${img5})`, height: '100vh', position:'absolute', top: '0', bottom: '0'}}></div>
-          </Parallax>
-        </ParallaxProvider> */}
-        <ParallaxProvider styles={{innherHeight : '200vh'}}>
+        <ParallaxProvider>
             <ParallaxBanner
               layers={[
-                {image: img11, speed: -20, scaleY: 5},
-                {image: img10, speed: -20, scaleY: 10},
-                {image: img9, speed: -20, scaleY: 15},
-                {image: img8, speed: -20, scaleY: 20},
-                {image: img7, speed: -20, scaleY: 25},
-                {image: img6, speed: -20, scaleY: 25},
-                {image: img5, speed: -20, scaleY: 27},
-                {image: img4, speed: -20, scaleY: 30},
-                {image: img3, speed: -20, scaleY: 31},
-                {image: img2, speed: -20, scaleY: 33},
-                {image: img1, speed: -20, scaleY: 36},
+                {image: l1, speed: -10, scaleY: 5},
+                {image: l2, speed: -10, scaleY: 30},
+                {image: l3, speed: -10, scaleY: 20},
+                {image: l4, speed: -10, scaleY: 20, style: {zIndex: 2}},
+                {image: l5, speed: -10, scaleY: 0, style:{zIndex: 3}},
               ]}
-              style={{height: '100vh' }}
-            />
+              style={{height: '100vh'}}
+            >
+              <div style={{position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, display: 'flex', alignItems: "center", justifyContent: "center", zIndex: 0}}>
+                <h1 style={{transform: 'translate(0, -100px)', fontSize: '4rem'}}>Kurisu</h1>
+              </div>
+            </ParallaxBanner>
         </ParallaxProvider>
-
-        <div style={{backgroundColor: '#00ff70', height: '200vh'}}></div>
+        <div style={{backgroundColor: '#020d13', height: '100vh'}}></div>
       </div>
     </>
   )
